89b60679ccff61a2082f2dc2894fc4a85b51c4f6
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-04-01 23:43:44 +0900

    함수형 인터페이스 + Map으로 구현

--- src/main/java/coordinate/FigureFactory.java
+++ src/main/java/coordinate/FigureFactory.java
@@ -1,19 +1,30 @@
 package coordinate;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
-public class FigureFactory { // test
+/**
+ * FigureFactory
+ * points의 size()에 따라 다른 객체를 생성해 반환한다.
+ */
+public class FigureFactory {
+
+    private static final Map<Integer, FigureCreator> creators = new HashMap<>(); // static 선언 필수
+
+    static {
+        creators.put(Line.LINE_POINT_SIZE, points -> new Line(points));       // method reference 변환 가능
+        creators.put(Rectangle.RECTANGLE_POINT_SIZE, points -> new Rectangle(points));
+        creators.put(Triangle.TRIANGLE_POINT_SIZE, points-> new Triangle(points));
+    }
+
     static Figure getInstance(List<Point> points) {
-        if (points.size() == Line.LINE_POINT_SIZE) {
-            return new Line(points);
-        }
-
-        if (points.size() == Triangle.TRIANGLE_POINT_SIZE) {
-            return new Triangle(points);
-        }
-
-        if (points.size() == Rectangle.RECTANGLE_POINT_SIZE) {
-            return new Rectangle(points);
+        FigureCreator creator = creators.getOrDefault(points.size(), null);
+        if (creator != null) {
+            return creator.create(points);
         }
 
         throw new IllegalArgumentException("유효하지 않은 도형입니다.");
